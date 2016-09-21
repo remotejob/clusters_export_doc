cfb2c8d123f31e88170d82f3090c1e32673e5140
juno <juno@krasava.fi>
2016-09-21 08:01:23 +0300

    dev

---
+++ packages.bash
@@ -0,0 +1,4 @@
+godoc -index -html github.com/remotejob/clusters_export/docker-gitsync
+godoc -index -html github.com/remotejob/clusters_export/docker-goreverseproxy
+godoc -index -html github.com/remotejob/clusters_export/docker-goreverseproxy/dbhandler
+godoc -index -html github.com/remotejob/clusters_export/docker-goreverseproxy/handlers
