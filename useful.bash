d2c76250468df48207f266871ddcb6d818f50250
juno <juno@krasava.fi>
2016-09-22 09:14:38 +0300

    dev

--- useful.bash
+++ useful.bash
@@ -1,7 +1,7 @@
 
 godoc -http ":6060" -goroot "/home/juno/neonworkspace/gowork/"
 
-wget -e robots=off -r -np -E -p  http://localhost:6060/pkg/github.com/remotejob/clusters_export
+wget -e robots=off -r -np -E -p  http://localhost:6060/pkg/github.com/remotejob
 
 
 go list ../clusters_export/./...
